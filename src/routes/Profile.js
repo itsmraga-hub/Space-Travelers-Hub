d95ee3871c9b80194c49b1e4f6d29d8724046808
Raga <ragawilliam570@gmail.com>
2022-09-27 19:27:45 +0300

    Add reserved Missions to Profile page

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,11 +1,31 @@
+/* eslint-disable camelcase */
+import { useSelector } from 'react-redux';
+import MissionSpace from '../components/MissionSpace';
+
 import style from './css/Profile.module.css';
 
-const Profile = () => (
-  <>
-    <main>
-      <h1 className={style.Header}>Profile</h1>
-    </main>
-  </>
-);
+const Profile = () => {
+  const missions = useSelector((state) => state.missions);
+
+  const reservedMissions = missions.filter((obj) => obj.reserved);
+
+  return (
+    <>
+      <main>
+        <div className={style.MyMissions}>
+          <h2 className={style.MissionHeader}>My Missions</h2>
+          <ul className={style.Missions}>
+            {
+              reservedMissions.map((mission) => {
+                const { mission_name, mission_id } = mission;
+                return <MissionSpace key={mission_id} name={mission_name} />;
+              })
+            }
+          </ul>
+        </div>
+      </main>
+    </>
+  );
+};
 
 export default Profile;
