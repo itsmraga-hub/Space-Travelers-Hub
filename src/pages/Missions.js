fbc93fb292ab770bb9b9d5c738043470f33f1f76
Raga <ragawilliam570@gmail.com>
2022-09-27 16:43:20 +0300

    Add eventlistener to listen for click and call joinMission

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -2,7 +2,7 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 
-import { fetchMissions } from '../redux/missions/missions';
+import { fetchMissions, joinMission } from '../redux/missions/missions';
 
 import Mission from '../components/Mission';
 
@@ -11,12 +11,16 @@ import style from './css/Missions.module.css';
 const Missions = () => {
   const missions = useSelector((state) => state.missions);
 
-  console.log(missions);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchMissions());
   }, []);
+
+  const join = (e) => {
+    e.preventDefault();
+    dispatch(joinMission(e.target.id));
+  };
 
   return (
     <section className={style.MissionSection}>
@@ -29,8 +33,19 @@ const Missions = () => {
         </li>
         {
         missions.map((mission) => {
-          const { mission_id, mission_name, description } = mission;
-          return <Mission key={mission_id} name={mission_name} desc={description} />;
+          const {
+            mission_id, mission_name, description, reserved,
+          } = mission;
+          return (
+            <Mission
+              key={mission_id}
+              id={mission_id}
+              name={mission_name}
+              desc={description}
+              join={join}
+              isReserved={reserved}
+            />
+          );
         })
       }
       </ul>
