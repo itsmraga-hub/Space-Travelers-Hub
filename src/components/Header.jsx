32dc0f8a40aca0e687e253bbdfc16b33d286fea2
Raga <ragawilliam570@gmail.com>
2022-09-27 13:43:54 +0300

    Add class names to Header elements to avoid styling conflicts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,10 +8,10 @@ const Header = () => (
         <img src="/images/planet.png" className="logo" alt="logo" />
         <Link to="/" className="brand-logo">Space Travelers&apos; Hub</Link>
       </div>
-      <ul>
-        <li><NavLink to="/rockets" className="link" activeclassname="active">Rockets</NavLink></li>
-        <li><NavLink to="/missions" className="link" activeclassname="active">Missions</NavLink></li>
-        <li><NavLink to="/my-profile" className="link" activeclassname="active">My Profile</NavLink></li>
+      <ul className="nav-link-container">
+        <li className="nav-link"><NavLink to="/rockets" className="link" activeclassname="active">Rockets</NavLink></li>
+        <li className="nav-link"><NavLink to="/missions" className="link" activeclassname="active">Missions</NavLink></li>
+        <li className="nav-link"><NavLink to="/my-profile" className="link" activeclassname="active">My Profile</NavLink></li>
       </ul>
     </nav>
   </header>
