ce6069d9e14366491a9425b546b2ba661a4b42ae
Raga <ragawilliam570@gmail.com>
2022-09-27 16:43:47 +0300

    Render component depending on reserved key

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,13 +1,23 @@
 const Mission = (props) => {
   const mission = props;
-  const { name, desc } = mission;
+  const {
+    name, desc, id, join, isReserved,
+  } = mission;
 
   return (
     <li>
       <h2>{ name }</h2>
       <p>{ desc }</p>
-      <a href="./">Not a member</a>
-      <button type="button">Join Mission</button>
+      <a rel="noreferrer" href="##">
+        {
+        isReserved ? 'Active Member' : 'Not A Member'
+      }
+      </a>
+      <button type="button" id={id} onClick={join}>
+        {
+          isReserved ? 'Leave Mission' : 'Join Mission'
+        }
+      </button>
     </li>
   );
 };
