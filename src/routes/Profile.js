998d6094d0402443c2a35fadca3f45b98725874b
niicode <niidarku66@gmail.com>
2022-09-28 08:47:57 +0000

    Display reserved Rockets in profile page

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -6,6 +6,7 @@ import style from './css/Profile.module.css';
 
 const Profile = () => {
   const missions = useSelector((state) => state.missions);
+  const rockets = useSelector((state) => state.rockets.rockets);
 
   const reservedMissions = missions.filter((obj) => obj.reserved);
 
@@ -19,6 +20,19 @@ const Profile = () => {
               reservedMissions.map((mission) => {
                 const { mission_name, mission_id } = mission;
                 return <MissionSpace key={mission_id} name={mission_name} />;
+              })
+            }
+          </ul>
+        </div>
+        <div className={style.MyMissions}>
+          <h2 className={style.MissionHeader}>My Rockets</h2>
+          <ul className={style.Missions}>
+            {
+              rockets.map((rocket) => {
+                if (rocket.reserved) {
+                  return <MissionSpace key={rocket.id} name={rocket.rocket_name} />;
+                }
+                return null;
               })
             }
           </ul>
